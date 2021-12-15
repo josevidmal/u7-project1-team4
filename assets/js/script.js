7cda33d6fb0687a97cf5be151fe82338e54d655d
Dinorah Abrego <dinoabrego@Dinorahs-MacBook-Air.local>
2021-12-15 13:58:14 -0600

    Script change with fucntion to get basic info. Small changes to css and html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,15 +11,39 @@ searchBtn.addEventListener("click", function(event) {
     event.preventDefault();
 
 var country = destinationInputEl.value;
-var restCountriesQuery = "https://restcountries.com/v3.1/name/" + country;
+var restCountriesQuery = "https://restcountries.com/v2/name/" + country;
 
 fetch(restCountriesQuery)
     .then(function (response1) {
         return response1.json();
     })
-    .then(function(data1) {
-        console.log(data1);
+    .then(function(data) {
+        console.log(data);
+        getBasicInfo(data);
     })
 })
+
+function getBasicInfo(data) {
+    var basicInfoTitle = document.getElementById("basic-info-title");
+    basicInfoTitle.textContent = data[0].name + "   ";
+
+    //add flag icon
+    var flagIconUrl = data[0].flag;
+    var flagIcon = document.createElement("img");
+    flagIcon.src = flagIconUrl;
+    flagIcon.setAttribute('width', '50px');
+    flagIcon.setAttribute('height', '50px');
+    basicInfoTitle.appendChild(flagIcon);
+
+    //Basic Info
+    basicInfoEl.textContent = "Official Name: " + data[0].altSpellings[2] + "\r\n Capital: " + data[0].capital + "\r\n Language: " + data[0].languages[0].name;
+    
+    //Currency Info
+    var currencyElTitle = document.getElementById("currency-title");
+    currencyElTitle.textContent = data[0].currencies[0].name;
+    
+    // currencyEl is the element containg the code for the currency exchange
+    currencyEl.textContent = data[0].currencies[0].code;
+}
 
 var alphaQuery = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=demo";
